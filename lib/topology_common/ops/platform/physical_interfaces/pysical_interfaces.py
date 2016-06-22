f267d0163a4e3098de00fce4b77dc3950a14037b
Matthew Hall <matthew.hall@hpe.com>
2016-06-22 11:45:54 -0700

    new: dev: add wait for interfaces up function
    Change-Id: I531c793115f89289e159dcd75e227b5195943e62
    Signed-off-by: Matthew Hall <matthew.hall@hpe.com>

---
+++ lib/topology_common/ops/platform/physical_interfaces/pysical_interfaces.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+Common functions for pysical interfaces
+"""
+
+from time import sleep
+
+
+def wait_until_up(switch, portlbls):
+    """
+    Wait until the interface, as mapped by the given portlbl, is marked as up.
+
+    :param switch: The switch node.
+    :param str or list portlbls: Port label/s that is mapped to the interface/s
+    :return: None if interface is brought-up. If not, an assertion is raised.
+    """
+
+    if not isinstance(portlbls, list):
+            portlbls = [portlbls]
+
+    for portlbl in portlbls:
+        for i in range(30):
+            status = switch.libs.vtysh.show_interface(portlbl)
+            if status['interface_state'] == 'up':
+                break
+            sleep(1)
+        else:
+            assert False, (
+                'Interface {}:{} never brought-up after '
+                'waiting for 30 seconds'.format(
+                    switch.identifier, portlbl
+                )
+            )
+            
